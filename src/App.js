8869850edc9d1c703e10c7c35854b7b401f88d6b
theNewJson <jason5540105@gmail.com>
2019-10-28 21:45:08 +0800

    feat: update root

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import List from './articles'
+import ArticleCreate from './articles/create'
+import ArticleDetail from './articles/detail'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '80vw', margin: '10vh auto 10vh auto' }}>
+      <Switch>
+        <Route exact path='/articles' component={List} />
+        <Route exact path='/articles/create' component={ArticleCreate} />
+        <Route exact path='/articles/:id' component={ArticleDetail} />
+      </Switch>
     </div>
   );
 }
